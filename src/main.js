3a4b8ef1f70157abd42b7e8af378c1fb9404594e
beasou <beatrizreginasouza1@gmail.com>
2022-10-18 18:43:58 -0300

    Alteração de cor do cartão

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+ccBgColor01.setAttribute("fill", "pink")
+ccBgColor02.setAttribute("fill", "purple")
+
