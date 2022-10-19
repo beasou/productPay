9458cb3f122979e2573445a9c74e4f0b7834bed2
beasou <beatrizreginasouza1@gmail.com>
2022-10-19 11:27:00 -0300

    função troca de bandeira do cartão

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import "./css/index.css"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-ccBgColor01.setAttribute("fill", "pink")
-ccBgColor02.setAttribute("fill", "purple")
+function setCardType(type){
+    const colors = {
+        "visa": ["#436D99","#2D57F2"],
+        "mastercard": ["#C69347", "#EB5757"],
+        "dinersclubinternational": ["#234B8D", "#BDBDBD"],
+        "mastercardcircus": ["#234B8D","#0099DF"],
+        "default":["black", "gray"]
+    }
+    ccBgColor01.setAttribute("fill", colors[type][0])
+    ccBgColor02.setAttribute("fill", colors[type][1])
+    ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
 
+setCardType("mastercardcircus")
